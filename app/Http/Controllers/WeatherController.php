074684300117caecfddc2812d4aff1d4c3d24fea
Ilies-Bouderba <bouderbailies2@gmail.com>
2025-02-26 16:15:50 +0100

    ``` No code changes made. ```

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -6,6 +6,7 @@ use App\Models\Weather;
 use App\Http\Requests\StoreWeatherRequest;
 use App\Http\Requests\UpdateWeatherRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class WeatherController extends Controller
 {
@@ -40,14 +41,52 @@ class WeatherController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Weather $weather)
+    public function show(String $city)
     {
-        return $weather;
+        $response = Http::withHeaders([
+            'User-Agent' => 'Weather Api',
+        ])->get('https://nominatim.openstreetmap.org/search', [
+            'q' => $city,
+            'format' => 'json',
+            'limit' => 1
+        ]);
+
+        if ($response->failed()) {
+            return response()->json(['error' => 'Failed to fetch data'], 500);
+        }
+
+        $data = $response->json();
+
+        if (empty($data)) {
+            return response()->json(['error' => 'City not found'], 404);
+        }
+
+        $weather = Http::withHeaders([
+            'User-Agent' => 'Weather Api',
+        ])->get('https://api.open-meteo.com/v1/forecast', [
+            'latitude' => $data[0]['lat'],
+            'longitude' => $data[0]['lon'],
+            'current' => 'temperature_2m,relative_humidity_2m',
+        ]);
+
+        if ($weather->failed()) {
+            return response()->json(['error' => 'Failed to fetch weather data'], 500);
+        }
+
+        $weatherData = $weather->json();
+
+        if (empty($weatherData)) {
+            return response()->json(['error' => 'Weather not found'], 404);
+        }
+
+        return [
+            "location" => $city,
+            "temperature" => $weatherData['current']['temperature_2m'] . "°C",
+            "humidity" => $weatherData['current']['relative_humidity_2m'] . '%',
+            "time" => $weatherData['current']['time'] . " UTC",
+        ];
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Weather $weather)
     {
         $fields = $request->validate([
